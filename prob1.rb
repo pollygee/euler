330b09849d216c7d2be53eb3bcfd8eea8527e076
“Polly Sileo“ <pollysileo@gmail.com>
2016-10-31 20:54:43 -0400

    Add specs

--- prob1.rb
+++ prob1.rb
@@ -3,9 +3,22 @@ require 'pry'
 # of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-# binding.pry
+RSpec.describe "Multiples of 3 and 5" do
+  describe 'a number 10' do
+    it 'has multiples that sum' do
+      expect(sum_multiples(10)).to eq(23)
+    end
+  end
 
-def find_multiples max
+  describe 'a number 1000' do
+    it 'has multiples that sum' do
+      expect(sum_multiples(1000)).to eq(233168)
+    end
+  end
+
+end
+
+def sum_multiples max
   ary = *(1...max)
   divisible_list = []
 
@@ -15,13 +28,14 @@ def find_multiples max
     end
   end
 
-  divisible_list
+  divisible_list.inject(:+)
 end
 
 def divisible number
   (number % 3 == 0) || (number % 5 == 0)
 end
 
-multiples = find_multiples 1000
-puts multiples
-puts multiples.inject(:+)
+# multiples = find_multiples 1000
+# puts multiples
+# puts multiples.inject(:+)
+
