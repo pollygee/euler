119b5ac77c82ffcd88a9a633eebd6f198222fcc0
“Polly Sileo“ <pollysileo@gmail.com>
2017-02-04 15:33:12 -0500

    WIP

--- prob4.rb
+++ prob4.rb
@@ -5,10 +5,14 @@
 
 RSpec.describe "Largest Palendrome Product Finder" do 
   it "returns the numbers that create the largest palendrom of 2 numbers" do
-    expect(palendrome_finder(2).to eq())
+    expect(palendrome_finder(2).to eq(9009))
   end
 end
 
 def palendrome_finder(number_of_digits)
-  
+  max_number = (10 ** number_of_digits) - 1
+  number_one = max_number
+  number_two = max_number
+
+
 end
